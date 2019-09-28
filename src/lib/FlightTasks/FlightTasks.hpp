7f563ec21d6edee4aa4bd25b7017442871423a5c
Daniel Agar <daniel@agar.ca>
2019-09-28 13:42:15 -0400

    FlightTasks: switch to uORB::Subscription

--- src/lib/FlightTasks/FlightTasks.hpp
+++ src/lib/FlightTasks/FlightTasks.hpp
@@ -42,7 +42,6 @@
 #pragma once
 
 #include "FlightTask.hpp"
-#include "SubscriptionArray.hpp"
 #include "FlightTasks_generated.hpp"
 #include <lib/WeatherVane/WeatherVane.hpp>
 #include <uORB/PublicationQueued.hpp>
@@ -168,8 +167,6 @@ private:
 		FlightTaskIndex index;
 	};
 	flight_task_t _current_task = {nullptr, FlightTaskIndex::None};
-
-	SubscriptionArray _subscription_array;
 
 	struct task_error_t {
 		int error;
